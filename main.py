220e7323265e016d63058896b31a6ac7636ac6ab
Lewis Bates <lewisbates2008@outlook.com>
2026-05-17 18:30:49 +0100

    feat:Added Sidebar - todo: complete sidebar buttons + styling

--- main.py
+++ main.py
@@ -3,38 +3,77 @@ import tkinter as tk
 #each frame
 class App(tk.Tk):
     def __init__(self):
+        #=====ROOT FRAME CREATION==========
         super().__init__() # calls constructors for tk instance to create a root frame
         #Sets attributes for root frame using setters
         self.title("Quiz Builder")
         self.geometry("800x500")
-
+        #====GLOBAL VARIABLES====
         self.score=0 
         self.qsanswered= 0
-
-
+        self.sbtoggle=True
+        self.frames={}
+        #=======MAIN SUBFRAME==================
         #creates a containter which stores all frames
         container= tk.Frame(self)
-        container.grid(row=0, column=0, sticky="nsew")
+        container.grid(row=0, column=1, sticky="nsew")
         
+        #======GRID CONFIG=========
         #tells the grid to expand into the space left by the root frame
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
         container.grid_rowconfigure(0,weight=1)
         container.grid_columnconfigure(0,weight=1)
-
-        self.frames={}
-        # creates an instance of the frame, passing in the container frame as a a base 
+        
+        #=====SIDEBAR=========
+        self.sidebar=tk.Frame(self,width=200,background="#5276F6")
+        self.sidebar.grid(row=0,column=0,sticky="ns")
+        self.sidebar.propagate(False)
+        tk.Label(self.sidebar,text="Quiz Builder", font= ("Arial", 18 , "underline"),background="#5276F6"
+        ).pack(pady=30,padx=10)
+        #Log Out Button
+        log=tk.Button(self.sidebar,text="Log Out",command=lambda:self.logOut(),background="#183EC6",)
+        log.pack(pady=28)
+        self.toggle=tk.Button(self,text="Menu",command=lambda:self.togglesidebar(sidebar=self.sidebar))
+        self.toggle.grid(row=0,column=0,sticky="n")
+        
+        
+        # creates an instance of the frame, passing in the container frame as a base 
         #and the current class(App) as a controller to allow for manipulation through the class
         for i in (Login,Dashboard,Quiz,Progress):
-            frame=i(container,self)
+            frame=i(parent=container,controller=self)
             self.frames[i]=frame # store the frame using self.frames
             frame.grid(row=0,column=0,sticky="nsew") # place each frame
         #method used to present a frame my raising it to the top of the stack
         self.showFrame(Login)
+        self.sidebar.grid_forget()
+        self.toggle.grid_forget()
+            
     def showFrame(self,frame):
         f= self.frames[frame]
         f.tkraise()
-
+    def removeSidebar(self):
+        self.toggle.grid_forget()
+        self.sidebar.grid_forget()
+    def createSidebar(self):
+        self.toggle.grid(row=0,column=0,sticky="n")
+        self.sidebar.grid(row=0,column=0,sticky="ns")
+    def logOut(self):
+        loginframe=self.frames[Login]
+        loginframe.username.delete(0,tk.END)
+        loginframe.password.delete(0,tk.END)
+        self.showFrame(Login)
+        self.removeSidebar()
+    
+    #CREATE GLOBAL SIDEBAR  
+    def togglesidebar(self,sidebar):
+            if self.sbtoggle==True:
+                sidebar.grid_forget()
+                self.sbtoggle=False
+            else:
+                sidebar.grid(row=0,column=0,sticky="ns")
+                self.sbtoggle=True
+            
 #Creates the login screen using the frame class
 class Login(tk.Frame):
     def __init__(self,parent,controller):
@@ -47,23 +86,23 @@ class Login(tk.Frame):
             self.grid_rowconfigure(i,weight=1)
             self.grid_columnconfigure(i,weight=1)
         #creates a sub frame   
-        container=tk.Frame(self)
+        self.container=tk.Frame(self)
         #places in the middle of the current frame. I then place each widget in this sub frame so that they are centred
-        container.grid(row=2,column=2)
-        self.label=tk.Label(container, text= "Login", font=("Arial",20)).grid(row=0, column=1)
+        self.container.grid(row=2,column=2)
+        self.label=tk.Label(self.container, text= "Login", font=("Arial",20)).grid(row=0, column=1)
         #Displays username text and allows for text entry
-        tk.Label(container, text="Username").grid(row=1,column=0)
-        self.username=tk.Entry(container)
+        tk.Label(self.container, text="Username").grid(row=1,column=0)
+        self.username=tk.Entry(self.container)
         self.username.grid(row=1,column=1)
         
 
         #Displays password text and allows for entry of data
-        tk.Label(container, text="Password").grid(row=2,column=0)
-        self.password = tk.Entry(container, show="*")
+        tk.Label(self.container, text="Password").grid(row=2,column=0)
+        self.password = tk.Entry(self.container, show="*",)
         self.password.grid(row=2,column=1)
         
         #calls function to simulate login validation and move to dash frame
-        tk.Button(container,
+        tk.Button(self.container,
                   text="Login",
                   command=lambda:self.loginbutton(self.controller)
                 ).grid(row=3,column=1)   
@@ -73,8 +112,11 @@ class Login(tk.Frame):
     def loginbutton(self,controller):
         if self.username.get()=="LEWIS" and self.password.get()=="171008":
             controller.showFrame(Dashboard)
+            controller.createSidebar()
+            self.loginmessage.grid_forget()
         else:
-            tk.Label(self,text="Invalid login details",font=("Helvetica,18")).grid(row=5,column= 2)
+            self.loginmessage=tk.Label(self.container,text="Invalid login details",font=("Helvetica,18"))
+            self.loginmessage.grid(row=4,column= 1,ipady=10)
 
 
 #Quiz Frame
